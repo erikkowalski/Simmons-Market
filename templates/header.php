a8a6911b4972eae83beaf482d89947cf93dce09e
erikkowalski <erikkowalski@mac.com>
2015-06-27 14:04:15 -0400

    made navar rixed, floated links to right, changed nav height to 100px and adding global padding to body to offset

--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,4 @@
-<header class="banner navbar navbar-default navbar-static-top" role="banner">
+<header class="banner navbar navbar-default navbar-fixed-top" role="banner">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
@@ -10,7 +10,7 @@
             <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
         </div>
 
-        <nav class="collapse navbar-collapse" role="navigation">
+        <nav class="collapse navbar-collapse navbar-right" role="navigation">
             <?php
 if (has_nav_menu('primary_navigation')) :
 wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav']);
