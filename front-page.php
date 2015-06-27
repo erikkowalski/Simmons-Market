eff911f164dc9a2c4a5c39b2d3332b100ae0a017
erikkowalski <erikkowalski@mac.com>
2015-06-27 13:04:10 -0400

    initial commit

---
+++ front-page.php
@@ -0,0 +1,20 @@
+
+
+<img class="img-responsive" src="<?php  the_field('home_img') ?>" alt="">
+
+<?php if (!have_posts()) : ?>
+<div class="alert alert-warning">
+    <?php _e('Sorry, no results were found.', 'sage'); ?>
+</div>
+<?php get_search_form(); ?>
+<?php endif; ?>
+
+<?php while (have_posts()) : the_post(); ?>
+<?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+<?php endwhile; ?>
+
+<?php the_posts_navigation(); ?>
+
+<section class="photo-grid">
+    <div class="col-sx-2 "></div>
+</section>
