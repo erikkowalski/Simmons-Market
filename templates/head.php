f9bd478b89b91bd2ad29b16be9cb09b929a90aee
erikkowalski <erikkowalski@mac.com>
2015-06-27 14:01:32 -0400

    added web fonts- styling for footer and side bar, background image for nav bar

--- templates/head.php
+++ templates/head.php
@@ -4,6 +4,5 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700,600,400' rel='stylesheet' type='text/css'>
-    <?php wp_head(); ?>
+	  <link href='http://fonts.googleapis.com/css?family=Lato:400,700,400italic' rel='stylesheet' type='text/css'>    <?php wp_head(); ?>
   </head>
