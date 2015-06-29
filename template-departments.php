136a89286baddf25e230412df0720810c3b9bbfb
erikkowalski <erikkowalski@mac.com>
2015-06-29 18:13:04 -0400

    ajusted styling for departments header, optimized for larger devices,removed uneccesary row class

--- template-departments.php
+++ template-departments.php
@@ -7,15 +7,19 @@
 
 <?php while (have_posts()) : the_post(); ?>
 <?php get_template_part('templates/page', 'header'); ?>
+<div class="dashes"></div>
 <div class="container">
-	<div class="row">
-		<div class="col-xs-4 col-md-4">
-			<img class="img-responsive" src="<?php the_field('header_image'); ?>" alt="">
+
+		<div class="hidden-xs col-sm-4">
+			<img class="dept-header-img img-responsive" src="<?php the_field('header_image'); ?>" alt="">
 		</div>
-		<div class="col-xs-8 col-md-8">
+		<div class=" col-sm-8">
 			<?php get_template_part('templates/content', 'page'); ?>
 		</div>
-	</div>
 </div>
 <?php endwhile; ?>
+
+<div class="container">
+<hr>
+	</div>
 </section>
