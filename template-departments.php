ec8ed0a96cd7b7b4c34a95c7ce65a4e59c596d35
erikkowalski <erikkowalski@mac.com>
2015-07-01 14:32:27 -0400

    added padding to dept page, .page-header and make light box 2 columon on mobil devices

--- template-departments.php
+++ template-departments.php
@@ -22,10 +22,10 @@
 <div class="container dept-promos ">
 <hr>
 	<h3 class="lightbox-headline"><?php the_field('light_box_headline') ?></h3>
-	<div class="col-md-3"><a href="<?php the_field('lightbox_one_link'); ?>" data-lightbox="image-set" data-title="<?php the_field('lightbox_image_caption'); ?>"><img class="img-responsive" src="<?php the_field('lightbox_one'); ?>" alt="<?php the_field('light_box_image_description'); ?>"  ></a></div>
-	<div class="col-md-3"><a href="<?php the_field('lightbox_image_two_link'); ?>" data-lightbox="image-set" data-title="<?php the_field('lightbox_image_two_caption'); ?>"><img class="img-responsive" src="<?php the_field('lightbox_two'); ?>" alt="<?php the_field('light_box_image_two_description'); ?>" ></a></div>
-	<div class="col-md-3"><a href="<?php the_field('lightbox_image_three_link'); ?>" data-lightbox="image-set" data-title="<?php the_field('lightbox_image_three_caption'); ?>"><img class="img-responsive" src="<?php the_field('lightbox_three'); ?>" alt="<?php the_field('light_box_image_three_description'); ?>" ></a></div>
-	<div class="col-md-3"><a href="<?php the_field('lightbox_image_four_link'); ?>" data-lightbox="image-set" data-title="<?php the_field('lightbox_image_four_caption'); ?>"><img class="img-responsive" src="<?php the_field('lightbox_four'); ?>" alt="<?php the_field('light_box_image_four_description'); ?>" ></a></div>
+	<div class="col-xs-6 col-md-3"><a href="<?php the_field('lightbox_one_link'); ?>" data-lightbox="image-set" data-title="<?php the_field('lightbox_image_caption'); ?>"><img class="img-responsive" src="<?php the_field('lightbox_one'); ?>" alt="<?php the_field('light_box_image_description'); ?>"  ></a></div>
+	<div class="col-xs-6 col-md-3"><a href="<?php the_field('lightbox_image_two_link'); ?>" data-lightbox="image-set" data-title="<?php the_field('lightbox_image_two_caption'); ?>"><img class="img-responsive" src="<?php the_field('lightbox_two'); ?>" alt="<?php the_field('light_box_image_two_description'); ?>" ></a></div>
+	<div class="col-xs-6 col-md-3"><a href="<?php the_field('lightbox_image_three_link'); ?>" data-lightbox="image-set" data-title="<?php the_field('lightbox_image_three_caption'); ?>"><img class="img-responsive" src="<?php the_field('lightbox_three'); ?>" alt="<?php the_field('light_box_image_three_description'); ?>" ></a></div>
+	<div class="col-xs-6 col-md-3"><a href="<?php the_field('lightbox_image_four_link'); ?>" data-lightbox="image-set" data-title="<?php the_field('lightbox_image_four_caption'); ?>"><img class="img-responsive" src="<?php the_field('lightbox_four'); ?>" alt="<?php the_field('light_box_image_four_description'); ?>" ></a></div>
 
 
 	</div>
